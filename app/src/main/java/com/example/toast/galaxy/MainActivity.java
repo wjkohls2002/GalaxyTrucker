5446e141ea25dbdc1f25be484e864f17c25d9f7d
wjkohls2002 <wjkohls2002@gmail.com>
2017-08-03 19:09:48 -0400

    Add back button

--- app/src/main/java/com/example/toast/galaxy/MainActivity.java
+++ app/src/main/java/com/example/toast/galaxy/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
